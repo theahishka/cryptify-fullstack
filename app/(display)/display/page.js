015ee4d6c20cf73de2b0233e9f6b530f61cabd91
theahishka <abdullafaizov@gmail.com>
2023-07-02 01:13:42 +0400

    changed back to one fetch function

--- app/(display)/display/page.js
+++ app/(display)/display/page.js
@@ -3,40 +3,55 @@ import Display from "@/components/display/display";
 // const API_ORIGIN = "http://localhost:3001";
 const API_ORIGIN = "https://www.thecryptify.io";
 
-async function getCryptoSpreads() {
-	const cryptoSpreadsRaw = await fetch(
-		`${API_ORIGIN}/api/cryptify-info/display/spreads`,
+async function getCryptoSpreadsAndRates() {
+	const spreadsAndRatesRaw = await fetch(
+		`${API_ORIGIN}/api/cryptify-info/display/spreads-and-rates`,
 		{
 			cache: "no-store",
 		}
 	);
 
-	const cryptoSpreads = await cryptoSpreadsRaw.json();
+	const spreadsAndRates = await spreadsAndRatesRaw.json();
 
-	return cryptoSpreads;
+	return spreadsAndRates;
 }
 
-async function getCryptoRates() {
-	const cryptoRatesRaw = await fetch(
-		`${API_ORIGIN}/api/cryptify-info/display/rates`,
-		{
-			cache: "no-store",
-		}
-	);
+// async function getCryptoSpreads() {
+// 	const cryptoSpreadsRaw = await fetch(
+// 		`${API_ORIGIN}/api/cryptify-info/display/spreads`,
+// 		{
+// 			cache: "no-store",
+// 		}
+// 	);
 
-	const cryptoRates = await cryptoRatesRaw.json();
+// 	const cryptoSpreads = await cryptoSpreadsRaw.json();
 
-	return cryptoRates;
-}
+// 	return cryptoSpreads;
+// }
+
+// async function getCryptoRates() {
+// 	const cryptoRatesRaw = await fetch(
+// 		`${API_ORIGIN}/api/cryptify-info/display/rates`,
+// 		{
+// 			cache: "no-store",
+// 		}
+// 	);
+
+// 	const cryptoRates = await cryptoRatesRaw.json();
+
+// 	return cryptoRates;
+// }
 
 export default async function DisplayPage() {
-	const cryptoRates = await getCryptoRates();
-	const cryptoSpreads = await getCryptoSpreads();
+	// const cryptoRates = await getCryptoRates();
+	// const cryptoSpreads = await getCryptoSpreads();
 
-	let cryptoSpreadsAndRates = {
-		cryptoRates: cryptoRates,
-		cryptoSpreads: cryptoSpreads,
-	};
+	const cryptoSpreadsAndRates = await getCryptoSpreadsAndRates();
+
+	// let cryptoSpreadsAndRates = {
+	// 	cryptoRates: cryptoRates,
+	// 	cryptoSpreads: cryptoSpreads,
+	// };
 
 	return <Display cryptoSpreadsAndRates={cryptoSpreadsAndRates} />;
 }
